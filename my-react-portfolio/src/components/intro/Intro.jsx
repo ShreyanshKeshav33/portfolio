2c8e99d27fcd66282b75a41f84c4e037a684bd72
Shreyansh Keshav <71323239+ShreyanshKeshav33@users.noreply.github.com>
2023-03-13 19:52:52 +0530

    Update Intro.jsx

--- my-react-portfolio/src/components/intro/Intro.jsx
+++ my-react-portfolio/src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/man.png" alt="" /> //random photo
+          <img src="assets/wd.png" alt="" /> //random photo
         </div>
       </div>
       <div className="right">
